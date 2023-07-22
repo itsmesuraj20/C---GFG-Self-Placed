8672546d59fb98027e938f1cd95337c11b7ae18a
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2023-07-22 16:41:54 +0530

    All practice session from the second video of GFG

--- Recursion/first.cpp
+++ Recursion/first.cpp
@@ -2,15 +2,29 @@
 #include<iostream>
 using namespace std;
 
-void fun(int n);
+int fun1(int n)
+{
+    if (n == 1) {
+        return 0;
+    } else {
+        return 1 + fun1(n / 2);
+    }
+}
 
+
+//void fun(int n);
+// void fun1(int n);//another example for the recursion
 int main()
 {
-  fun(3);
+  //fun(3);
+
+  cout<<"Fun1 begin from here"<<endl;
+
+  cout<<fun1(16);
   return 0;  
 }
 
-void fun(int n)
+/*void fun(int n)
 {
     if(n==0)
     return ;
@@ -18,4 +32,17 @@ void fun(int n)
     cout<<n<<endl;
     fun(n-1);
     cout<<n<<endl;
-}
+}*/
+
+// void fun1(int n)
+// {
+//     if(n==0)
+//     return ;
+
+
+//     fun1(n-1);
+//     cout<<n<<endl;
+//     fun1(n-1);
+// }
+
+
